2f40e8c0351df5c682cde9ac30d12ebcbf4d5dc0
Mingull <60094450+Mingull@users.noreply.github.com>
2024-06-23 19:49:03 +0200

    added port variable to the db connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,9 +5,10 @@ dotenv.config();
 export const connection = await mysql
 	.createPool({
 		host: process.env.DB_HOST,
+		port: parseInt(process.env.DB_PORT as string),
 		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_DATABASE,
+		password: process.env.DB_PASS,
+		database: process.env.DB_NAME,
 		connectionLimit: 10,
 		waitForConnections: true,
 		queueLimit: 0,
