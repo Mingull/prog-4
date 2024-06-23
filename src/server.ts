82e9d26d10a40ac6f2d698e50189ece86ce93f49
Mingull <60094450+Mingull@users.noreply.github.com>
2024-06-23 20:08:38 +0200

    update

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
 	console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
 	next();
 });
+
 app.get("/", (req, res) => {
 	res.redirect("/api/info");
 });
